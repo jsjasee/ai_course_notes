98a0515ec9500495424bc80b4d1ee7678a2d9113
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-05-19 22:30:27 +0800

    add day 3 notes

--- Week6/pricer/evaluator.py
+++ Week6/pricer/evaluator.py
@@ -14,7 +14,9 @@ RED = "\033[91m"
 RESET = "\033[0m"
 COLOR_MAP = {"red": RED, "orange": YELLOW, "green": GREEN}
 
-WORKERS = 5
+WORKERS = (
+    5  # we are running this in parallel, spawns 5 threads that runs at the same time.
+)
 DEFAULT_SIZE = 200
 
 
@@ -33,7 +35,12 @@ class Tester:
 
     @staticmethod
     def make_title(predictor) -> str:
-        return predictor.__name__.replace("__", ".").replace("_", " ").title().replace("Gpt", "GPT")
+        return (
+            predictor.__name__.replace("__", ".")
+            .replace("_", " ")
+            .title()
+            .replace("Gpt", "GPT")
+        )
 
     @staticmethod
     def post_process(value):
@@ -59,7 +66,11 @@ class Tester:
         truth = datapoint.price
         error = abs(guess - truth)
         color = self.color_for(error, truth)
-        title = datapoint.title if len(datapoint.title) <= 40 else datapoint.title[:40] + "..."
+        title = (
+            datapoint.title
+            if len(datapoint.title) <= 40
+            else datapoint.title[:40] + "..."
+        )
         return title, guess, truth, error, color
 
     def chart(self, title):
@@ -133,7 +144,9 @@ class Tester:
         ]
 
         # 95% confidence interval for mean
-        ci = [1.96 * (sd / math.sqrt(i)) if i > 1 else 0 for i, sd in zip(x, running_stds)]
+        ci = [
+            1.96 * (sd / math.sqrt(i)) if i > 1 else 0 for i, sd in zip(x, running_stds)
+        ]
         upper = [m + c for m, c in zip(running_means, ci)]
         lower = [m - c for m, c in zip(running_means, ci)]
 
